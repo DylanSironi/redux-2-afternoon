365952d322f69a83297ceaeb6c6de18523c75060
DylanSironi <sironidylan@gmail.com>
2020-10-05 16:27:44 -0500

    work-in-progress

---
+++ src/ducks/userReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    email: null,
+    firstNName: null,
+    lastName: null
+}
+
+function reducer(state = initialState, action){
+    return state
+}
+export default reducer
